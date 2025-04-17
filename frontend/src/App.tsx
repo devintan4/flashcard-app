fe3739d5af4a9a1ff84b31f2b69b98caaeeaf608
Devin Tan <vninecoc1@gmail.com>
2025-04-17 21:10:57 +0700

    create frontend configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="w-full bg-black h-10 text-white">TES</div>
+    </div>
+  );
+};
+
+export default App;
